86fda688c88d14425e883f9c045bc66a5a6e42e9
afdhali <afdhali.apreto@gmail.com>
2025-02-04 12:16:18 +0700

    initial2

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import BookLists from "@/components/BookLists";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants/datadummy";
 
 export default function Home() {
   return (
-    <div className="">
-      <Button>Click Me</Button>
-    </div>
+    <>
+      <BookOverview {...(sampleBooks[0] as unknown as Book)} />
+      <BookLists
+        title="Latest Book"
+        books={sampleBooks.slice(1) as unknown as Book[]}
+        containerClassName="mt-28"
+      />
+    </>
   );
 }
